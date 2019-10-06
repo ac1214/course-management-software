e73062642319c3c49f733629306933b9daf6a979
ac1214 <32654899+ac1214@users.noreply.github.com>
2019-10-06 13:27:55 -0600

    Adding working test structure (#7)
    * add working test structure
    
    * update test info

---
+++ src/test/java/coursesoftware/BaseWindowTest.java
@@ -0,0 +1,13 @@
+package coursesoftware;
+
+import org.junit.*;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class BaseWindowTest {
+    @Test
+    public void testTest() {
+        assertEquals(30, 27 + 3);
+    }
+}
